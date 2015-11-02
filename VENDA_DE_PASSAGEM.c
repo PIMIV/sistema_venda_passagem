5ca7d15fb8e12886fb689473d335b8628eddde3d
vinis13 <cruzmendes@live.com>
2015-11-02 13:08:45 -0200

    aumento de casos
    ajuste no calculo final (contador idosos ainda aprsenta erro, pois nao
    pode passar de 2)

--- VENDA_DE_PASSAGEM.c
+++ VENDA_DE_PASSAGEM.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h> 
 
 
 
+     	
 
 int main()
 {
@@ -15,13 +17,24 @@ setlocale(LC_ALL, "Portuguese");
 	printf("=========================================================================\n");
 	system("color CF");
 	
-     int a[12][4], l, c, menu, soma, qtd, lugar, t, qa;
-     qtd = 0;
+     int a[9][4], l, c, menu, soma, qtd, lugar,  qa;
+     double meia, inteira, idoso,t;
+     char psg;
+     
+    
+	 
+
+	 qtd = 0;
      soma = 0;
      qa = 0;
      menu = 0;
      t = 0;
-     for (l=0;l<12;l++)
+     
+     meia = 0;
+     inteira = 0;
+     idoso = 0;
+     
+     for (l=0;l<9;l++)
      {
         for (c=0;c<4;c++)
         {
@@ -29,15 +42,17 @@ setlocale(LC_ALL, "Portuguese");
             a[l][c] = qtd;
             }
             }
-     while (menu!=6)
+     while (menu!=8)
      {
      printf("\n     ***************MENU***************");
      printf("\n     *1 - POLTRONAS OCUPADAS          *");
      printf("\n     *2 - POLTRONAS NA JANELA         *");
      printf("\n     *3 - POLTRONAS NO CORREDOR       *");
-     printf("\n     *4 - VENDER PASSAGEM             *");
-     printf("\n     *5 - TOTAL DE PASSAGENS VENDIDAS *");
-     printf("\n     *6 - SAIR                        *");
+     printf("\n     *4 - VENDER PASSAGEM Normal      *");
+     printf("\n     *5 - VENDER PASSAGEM Meia        *");
+     printf("\n     *6 - VENDER PASSAGEM Idoso       *");
+	 printf("\n     *7 - TOTAL DE PASSAGENS VENDIDAS *");
+     printf("\n     *8 - SAIR                        *");
      printf("\n     **********************************");
      printf("\n     DIGITE A OPCAO DESEJADA: ");
      scanf("%d", &menu);
@@ -45,7 +60,7 @@ setlocale(LC_ALL, "Portuguese");
      switch (menu)
      {
             case 1:
-                 for (l=0;l<12;l++)
+                 for (l=0;l<9;l++)
                  {
                      for (c=0;c<4;c++)
                      {
@@ -62,8 +77,9 @@ setlocale(LC_ALL, "Portuguese");
                               printf("\n");
                               }
             break;
+            
             case 2:
-                 for (l=0;l<12;l++)
+                 for (l=0;l<9;l++)
                  {
                      for (c=0;c<4;c++)
                      {
@@ -84,8 +100,9 @@ setlocale(LC_ALL, "Portuguese");
                               printf("\n");
                               }
             break;
+            
             case 3:
-                 for (l=0;l<12;l++)
+                 for (l=0;l<9;l++)
                  {
                      for (c=0;c<4;c++)
                      {
@@ -105,46 +122,111 @@ setlocale(LC_ALL, "Portuguese");
                                        printf("\n");
                                        }
             break;
+            
             case 4:
                  volta:
-                 printf("\nVALOR DA PASSAGEM R$15,00");
+                
+                
                  printf("\nDIGITE O NUMERO DA POLTRONA: ");
                  scanf("%d", &lugar);
-                 if (lugar>=1 && lugar<=48)
+                 if (lugar>=1 && lugar<=36)
                  {
                  soma++;
                  qa++;
-                 for (l=0;l<12;l++)
+                 for (l=0;l<9;l++)
                  {
                      for(c=0;c<4;c++)
                      {
                           if (a[l][c]==lugar)
                           {
                               a[l][c]=0;
+                              
                               }
                               }
                               }
                               }
                  else
                  {
-                     printf("\nAS POLTRONAS SÃO SOMENTE DE 1 A 48!!!\n");
+                     printf("\nAS POLTRONAS SÃO SOMENTE DE 1 A 36!!!\n");
                      goto volta;
                      }
             break;
+            
             case 5:
+                 
+            
+                
+                 printf("\nDIGITE O NUMERO DA POLTRONA:  ");
+                 scanf(" %d " , &lugar);
+                 
+                 
+            
+                 if (lugar>=1 && lugar<=36)
+                 {
+                 soma++;
+                 meia++;
+                 for (l=0;l<9;l++)
+                 {
+                     for(c=0;c<4;c++)
+                     {
+                          if (a[l][c]==lugar)
+                          {
+                              a[l][c]=0;
+                              
+                              }
+                              }
+                              }
+                              }
+                 else
+                 {
+                     printf("\nAS POLTRONAS SÃO SOMENTE DE 1 A 36!!!\n");
+                     
+                     }
+            break;
+            
+            case 6:
+                 
+                
+                
+                 printf("\nDIGITE O NUMERO DA POLTRONA: ");
+                 scanf("%d", &lugar);
+                 if (lugar>=1 && lugar<=36)
+                 {
+                 soma++;
+                 idoso++;
+                 for (l=0;l<9;l++)
+                 {
+                     for(c=0;c<4;c++)
+                     {
+                          if (a[l][c]==lugar)
+                          {
+                              a[l][c]=0;
+                              idoso++;
+                              }
+                              }
+                              }
+                              }
+                 else
+                 {
+                     printf("\nAS POLTRONAS SÃO SOMENTE DE 1 A 36!!!\n");
+                     
+                     }
+            break;
+            
+            case 7:
                  printf("\nVALOR DA PASSAGEM R$15,00");
-                 for (l=0;l<12;l++)
+                 for (l=0;l<9;l++)
                  {
                      for (c=0;c<4;c++)
                      {
                          if (a[l][c]==0)
                          {
-                         t = soma * 15;
+                         t = (qa*15)+(meia*7.5)+(idoso*0);
                          }
                          }
                          }
-                         printf("\nTOTAL DE PASSAGENS VENDIDAS %d", qa);
-                         printf("\nTOTAL R$%d,00\n", t);
+                         printf("\nTOTAL DE PASSAGENS VENDIDAS inteiras %4d meia %4f idoso %4f", qa, meia, idoso);
+                         printf("\nTOTAL R$%f\n", t);
              break;
              default :
                      printf("\nOPCAO INVALIDA\n");
